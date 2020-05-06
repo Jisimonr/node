b08505e23c156655e8be236d914d80ec4e3ed2f5
Nacho Simón Rodríguez <56724688+Jisimonr@users.noreply.github.com>
2020-05-06 19:55:08 +0200

    Otro

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
 response.writeHead(200, {"Content-Type": "text/plain"});
-response.end("Hola Nacho! Ops");
-response.end("!Lo has conseguido!");
+response.end("Hola Nacho!");
 });
 const port = process.env.PORT || 3000;
 server.listen(port);
