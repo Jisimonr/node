a16bf4f81e8b21ae3abc2f45da6f767b550a020d
Nacho Simón Rodríguez <56724688+Jisimonr@users.noreply.github.com>
2020-05-06 19:56:48 +0200

    Más caracteres

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
 response.writeHead(200, {"Content-Type": "text/plain"});
-response.end("Hola Nacho!");
+response.end("!Hola Nacho! Lo has conseguido");
 });
 const port = process.env.PORT || 3000;
 server.listen(port);
